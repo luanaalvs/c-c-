5b61818f8e14a61d2249c05cf7ab2135328e5860
Luana Silva <luanasilva.developer@gmail.com>
2026-06-08 21:46:34 -0300

    create io file

---
+++ src/io.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int num;
+
+  printf("Type a integer number: ");
+
+  scanf("%d", &num);
+
+  printf("Your number is: %d\n", num);
+
+  return 0;
+}
